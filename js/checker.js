4a6fe76ac6f7d591ac465c3b7d1fcaf31b31723e
apierc2175 <aidanwpierce@gmail.com>
2018-03-25 22:09:01 -0400

    Add files via upload

--- js/checker.js
+++ js/checker.js
@@ -1,3 +1,7 @@
+var img = new Image();
+var div = document.getElementById('img');
+
+
 function test() {
   var her = document.getElementById('er').value;
   var his = document.getElementById('is').value;
@@ -5,8 +9,12 @@ function test() {
   var hisR = his.toLowerCase();
   if(herL === "rayann"  & hisR === "aidan") {
     var laranja = "100%";
+    div.appendChild(img);
+    img.src = 'img/cat.jpg';
   } else {
     var laranja = "no chance holmes";
+    div.appendChild(img);
+    img.src = 'img/renato.png';
   }
   document.getElementById('result').innerHTML = laranja;
 }
